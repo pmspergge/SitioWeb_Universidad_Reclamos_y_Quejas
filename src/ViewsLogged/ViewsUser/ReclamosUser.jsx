856a123316349177d5f2d1f849119835f618c8c1
Matias Spergge <pmatiasspergge@gmail.com>
2022-08-29 13:07:20 -0300

    feat: tabla de quejas y reclamos usuario

--- src/ViewsLogged/ViewsUser/ReclamosUser.jsx
+++ src/ViewsLogged/ViewsUser/ReclamosUser.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import Table1 from "../../Components/UserTables/Table1";
 import FormReclamos from "../../Components/FormReclamos/FormReclamos";
+import data from "../../data/data.json";
+import ModalDetails from "../../Components/ModalDetails/ModalDetails";
 const ReclamosUser = () => {
+  const [detaisModal, setDetaisModal] = useState("");
+
+  const dataReclamos = data.filter((value) => value.type);
+  // reclamos
+  const numReclamos = () => {
+    return data.filter((value) => value.type).length;
+  };
   return (
     <>
       <div className="row my-2 shadow">
@@ -14,6 +24,11 @@ const ReclamosUser = () => {
               Información de todos los reclamos.
             </li>
           </ol>
+          <div className="d-grid">
+            <Table1 data={dataReclamos} textModal={setDetaisModal} />
+          </div>
+          {/* modal */}
+          <ModalDetails details={detaisModal} />
         </div>
       </div>
     </>
